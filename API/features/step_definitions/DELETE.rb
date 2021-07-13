b2566d386949b7020efc8ae59270fd14bc874b26
Ricardo Carteri Uehlein <ricardo_carteri@hotmail.com>
2021-07-13 12:17:18 -0300

    Remover debug

--- API/features/step_definitions/DELETE.rb
+++ API/features/step_definitions/DELETE.rb
@@ -3,6 +3,5 @@ Quando('realizar uma requisição DELETE passando o id') do
         Quando requerir todos os usuários
         Quando sortear id
     }
-    binding.pry
     @request_users = users.delete_user(@id)
 end
